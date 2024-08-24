d0c8a70617317e87ab471b94d6906eee6eb4d007
doiska <doiska@proton.me>
2024-08-24 20:06:43 -0300

    fix: reduce to 30 seconds restart

--- src/jobs/restart-server.ts
+++ src/jobs/restart-server.ts
@@ -2,17 +2,16 @@ import { broadcastGlobal, fetchApi } from '../lib/api.js';
 import { CronJob } from 'cron';
 
 export async function restartServer() {
-	await broadcastGlobal('Scheduling the server to be restarted in 60 seconds.');
+	await broadcastGlobal('Scheduling the server to be restarted in 30 seconds.');
 
 	await fetchApi('/shutdown', {
-		waittime: 60,
-		message: 'Server will be restarted in 60 seconds.'
+		waittime: 30,
+		message: 'Server will be restarted in 30 seconds.'
 	});
 }
 
 CronJob.from({
-	cronTime: '0 */1 * * *',
+	cronTime: '0 */3 * * *',
 	onTick: restartServer,
-	start: true,
-	runOnInit: true
+	start: true
 });
